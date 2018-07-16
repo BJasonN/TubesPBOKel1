6392d04871db41c580a74c714b4c07147be0aa11
gkevin9 <41147450+gkevin9@users.noreply.github.com>
2018-07-16 20:15:03 +0700

    benerin yang JLabel

--- TemplateTubes/src/view/TambahMatkul.java
+++ TemplateTubes/src/view/TambahMatkul.java
@@ -49,7 +49,7 @@ public class TambahMatkul extends JFrame{
         //--------------------------------
         
         lbltambahmatkul = new JLabel("Tambah Matkul");
-        lbltambahmatkul.setBounds(WIDTH, WIDTH, WIDTH, HEIGHT);
+        lbltambahmatkul.setBounds(450, -60, 100,200);
         pnlIsi.add(lbltambahmatkul);
         
         lblKodeMatkul = new JLabel("Kode Matkul : ");
